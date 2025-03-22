14be42863d41ed634fd1c59957cd848dc610616b
Momo <hutomowang@gmail.com>
2025-03-22 14:55:26 +0800

    Added some stuff about git add and git commit

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # git init untuk menggunakan directory sebagai folder project
 print("Hello world!")
+
+# git add [file] digunakan untuk memasukkan file ke dalam staged changes
+# staged changes sendiri adalah sebuah area dimana file2 yang telah diubah
+# akan ditaruh sebelum commit
