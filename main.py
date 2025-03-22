e73701a587b2030e2c8605822fb96a7978c06f06
Momo <hutomowang@gmail.com>
2025-03-22 14:57:25 +0800

    Added notes about saving for add and commit

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ print("Hello world!")
 # git add [file] digunakan untuk memasukkan file ke dalam staged changes
 # staged changes sendiri adalah sebuah area dimana file2 yang telah diubah
 # akan ditaruh sebelum commit
+
+# untuk save, pakai git commit -m "commit comment"
+# version control bisa lihat di git log
+# changes tidak akan muncul jika file tidak di save
